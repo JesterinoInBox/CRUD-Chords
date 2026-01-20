d7604fc71d9e631a6bb46d07d00776902a3ab8a8
JesterinoInBox <toliksankin@gmail.com>
2026-01-20 21:28:01 +0300

    Initial commit

---
+++ src/main/java/ru/Project/crud_chords/CrudChordsApplication.java
@@ -0,0 +1,13 @@
+package ru.Project.crud_chords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudChordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudChordsApplication.class, args);
+	}
+
+}
